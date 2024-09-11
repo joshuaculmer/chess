bb4a4e7e120415cac153539818d775370470b1a2
Joshua Culmer <joshuadculmer@gmail.com>
2024-09-11 09:54:10 -0600

    Added row, col variables and getters, toString, equals, hashCode functions for ChessPosition

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -8,7 +10,11 @@ package chess;
  */
 public class ChessPosition {
 
+    private final int row;
+    private final int col;
     public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
     /**
@@ -16,7 +22,7 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
 
     /**
@@ -24,6 +30,48 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return col;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + row +
+                "," + col +
+                ')';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ChessPosition that=(ChessPosition) o;
+
+        if (row != that.row) return false;
+        return col == that.col;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int result=row;
+        result=31 * result + col;
+        return result;
+    }
+
+
+
+    // Test functions for ChessPosition Class
+    public static void main(String[]args)
+    {
+        ChessPosition testA=new ChessPosition(0, 0);
+        ChessPosition testB=new ChessPosition(0,0);
+        ChessPosition testC=new ChessPosition(3,3);
+        System.out.print(testA);
+        System.out.println(testB);
+        if(testA.equals(testB))
+            System.out.printf("%s = %s%n",testA,testB);
+        if(testA.equals(testC))
+            System.out.printf("%s = %s%n :/",testA,testB);
     }
 }
