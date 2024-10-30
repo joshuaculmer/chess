ce4ecd330a58375e2eaee2b085ba65e826c4181c
Joshua Culmer <163606650+joshuaculmer@users.noreply.github.com>
2024-10-29 18:19:06 -0600

    Stub Implementation(UserDAOSQL tests)

--- server/src/test/java/service/UserDAOTests.java
+++ server/src/test/java/service/UserDAOTests.java
@@ -1,12 +1,14 @@
 package service;
 
+import dataaccess.UserDAO;
 import dataaccess.UserDAOMemory;
+import dataaccess.UserDAOSQL;
+import exception.ResponseException;
 import model.UserData;
 import org.eclipse.jetty.server.Authentication;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserDAOTests {
 
@@ -31,6 +33,36 @@ public class UserDAOTests {
 
     @Test
     public void clearUserDataMemory() {
+        UserData value1 = new UserData("default", "pw", "example");
+        UserData value2 = new UserData("test", "other", "gmail");
+        UserDAOMemory testdb = new UserDAOMemory();
+        testdb.addUserData(value1);
+        testdb.addUserData(value2);
+        UserData returned = testdb.getUserData(value1.username());
+        assertEquals(returned, value1);
+        testdb.clearUserData();
+        assertNull(testdb.getUserData(value1.username()));
+        assertNull(testdb.getUserData(value2.username()));
+    }
+
+    @Test
+    public void successGetUserDataSQL() {
+
+        UserData expected = new UserData("default", "pw", "example");
+        UserDAO testdb=null;
+        try {
+            testdb = new UserDAOSQL();
+        }
+        catch (ResponseException e) {
+            fail(e.getMessage());
+        }
+        testdb.addUserData(expected);
+        UserData returned = testdb.getUserData(expected.username());
+        assertEquals(returned, expected);
+    }
+
+    @Test
+    public void clearUserDataSQL() {
         UserData value1 = new UserData("default", "pw", "example");
         UserData value2 = new UserData("test", "other", "gmail");
         UserDAOMemory testdb = new UserDAOMemory();
