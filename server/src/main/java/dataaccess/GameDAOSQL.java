dca8c32fac847e5198a4698913f809d145bf8889
Joshua Culmer <163606650+joshuaculmer@users.noreply.github.com>
2024-10-30 16:02:18 -0600

    Implemented(clearGameData, create GameDB table), stubbed(addGame), need to refactor GameDAO to return int on addGame

--- server/src/main/java/dataaccess/GameDAOSQL.java
+++ server/src/main/java/dataaccess/GameDAOSQL.java
@@ -1,10 +1,20 @@
 package dataaccess;
 
+import com.google.gson.Gson;
+import exception.ResponseException;
 import model.GameData;
 
 import java.util.List;
 
+import static dataaccess.DatabaseManager.configureTable;
+import static dataaccess.DatabaseManager.executeUpdate;
+
 public class GameDAOSQL implements GameDAO{
+
+    public GameDAOSQL() throws ResponseException {
+        configureTable(createStatementGameDB);
+    }
+
     @Override
     public List<GameData> listGames() {
         return null;
@@ -12,6 +22,14 @@ public class GameDAOSQL implements GameDAO{
 
     @Override
     public void addGame(GameData game) {
+        String statement = "INSERT INTO gameDB (gameID, whiteUsername, blackUsername, gameName, json) VALUES (?, ?, ?, ?, ?)";
+        Object json = new Gson().toJson(game);
+        try {
+            int gameID = executeUpdate(statement, game.whiteUsername(), game.blackUsername(), game.gameName(), json);
+        }
+        catch (ResponseException ignored) {
+            System.out.println(ignored.messageToJSON());
+        }
 
     }
 
@@ -22,6 +40,24 @@ public class GameDAOSQL implements GameDAO{
 
     @Override
     public void clearGameData() {
-
+        String statement = "DELETE FROM gameDB";
+        try {
+            executeUpdate(statement);
+        }
+        catch (ResponseException ignored) {
+            System.out.println(ignored.messageToJSON());
+        }
     }
+
+    private final String[] createStatementGameDB = {
+            """
+            CREATE TABLE IF NOT EXISTS  gameDB (
+              `gameID` int NOT NULL AUTO_INCREMENT,
+              `whiteUsername` varchar(256),
+              `blackUsername` varchar(256),
+              `gameName` varchar(256),
+              `json` TEXT DEFAULT NULL
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
 }
